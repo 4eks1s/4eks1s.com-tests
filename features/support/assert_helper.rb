af79e7b9cdc2d92f52596f0961d3baac1c6ff771
4eks1s <35590285+4eks1s@users.noreply.github.com>
2018-08-03 22:55:32 +0300

    Updated tests to be compatable with next Webpage build

--- features/support/assert_helper.rb
+++ features/support/assert_helper.rb
@@ -1,6 +1,3 @@
 def assert_status_code(expected_code, response, action)
-  assert_equal(expected_code, response.code, "#{action} failed! Status code is not Correct
-       Expected: #{expected_code}
-       Received: #{response.code}
-       Response: #{response}")
+  assert_equal(expected_code, response.code, "#{action} failed! Status code is not Correct Expected: #{expected_code} Received: #{response.code} Response: #{response}")
 end
