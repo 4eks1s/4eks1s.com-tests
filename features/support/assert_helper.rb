9a0321420833747cd5ad4c4af059b7dc121a6671
4eks1s <35590285+4eks1s@users.noreply.github.com>
2018-08-03 23:46:07 +0300

    Debugging#1

--- features/support/assert_helper.rb
+++ features/support/assert_helper.rb
@@ -1,5 +1,6 @@
 require 'test-unit'
 
 def assert_status_code(expected_code, response, action)
+  puts "#{expected_code} got #{response.code}"
   assert_equal(expected_code, response.code, "#{action} failed! Status code is not Correct Expected: #{expected_code} Received: #{response.code} Response: #{response}")
 end
